4fbe00064e1211953cc7ce763b4a7f584247ed85
Jia Liu <proljc@gmail.com>
2026-06-28 13:17:31 +0800

    Add file header
    Signed-off-by: Jia Liu <proljc@gmail.com>

--- TinyGUI/src/MainWindow.cpp
+++ TinyGUI/src/MainWindow.cpp
@@ -1,3 +1,21 @@
+// TinyMLC - Tiny Machine Learning Compiler
+//
+// Copyright (c) 2026 Jia Liu & TinyMLC Contributors
+// SPDX-License-Identifier: Apache-2.0
+//
+// This file is part of TinyMLC.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "MainWindow.h"
 #include "ConsolePanel.h"
 #include "ConfigPanel.h"
@@ -619,7 +637,7 @@ void MainWindow::onClear() {
 
 void MainWindow::onAbout() {
     QMessageBox::about(this, tr("About TinyMLC"),
-        tr("TinyMLC Toolchain\n"
+        tr("TinyMLC\n"
            "Version 0.1.0\n\n"
            "Automatic Network Generator + Code Generator for MCU"));
 }
